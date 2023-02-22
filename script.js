ae03dee8ad081ef989082301c035309fb9451371
Gallues Matias <galluesmatias@gmail.com>
2023-02-21 21:01:17 -0300

    0,1

---
+++ script.js
@@ -0,0 +1,24 @@
+
+document.getElementById('botonMostrar').addEventListener('click', function () {
+
+    document.getElementById('exp').style.display = 'block';
+
+});
+
+document.getElementById('ocultar').addEventListener('click', function () {
+
+    document.getElementById('exp').style.display = 'none';
+
+});
+
+document.getElementById('botonMostrarH').addEventListener('click', function () {
+
+    document.getElementById('habilidades').style.display = 'block';
+
+});
+
+document.getElementById('ocultarH').addEventListener('click', function () {
+
+    document.getElementById('habilidades').style.display = 'none';
+
+});
